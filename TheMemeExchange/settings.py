820798f1cbf76fedb28c166f24cbdf48c1a37b27
ryan-beck <rbeck@csumb.edu>
2020-03-24 13:01:07 -0700

    trying to fix static files on heroku

--- TheMemeExchange/settings.py
+++ TheMemeExchange/settings.py
@@ -15,7 +15,7 @@ import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve(strict=True).parents[1]
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -136,18 +136,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    'var/www/static/',
-]
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
