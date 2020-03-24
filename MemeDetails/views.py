eedab0fafa17cb4f84525e1343b4dd4d1e616ad7
casscabrera <cass.cabrera99@gmail.com>
2020-03-23 19:31:59 -0700

    fix some safety bugs

--- MemeDetails/views.py
+++ MemeDetails/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
-from dbFunctions import getImageByID, addToCart as dbCart, getUserByID
+from dbFunctions import getImageByID, addToCart as dbCart, getUserByID, getOwnedMemes, getCart
 
 def index(request,image_id): #get actual ID from route
 	img = getImageByID(image_id)
-	context = {"image" : img, "user" : getUserByID(img.creator)}
+	context = {"image" : img, "user" : getUserByID(img.creator), "owned" : getOwnedMemes(request.user), "cart" : getCart(request.user)}
 	template = loader.get_template('details.html')
 	return HttpResponse(template.render(context, request))
 
@@ -13,7 +13,4 @@ def addToCart(request, image_id):
 	dbCart(request.user, image_id)
 	img = getImageByID(image_id)
 	context = {"image" : img, "user" : getUserByID(img.creator)}
-	template = loader.get_template('details.html')
-	return HttpResponse(template.render(context, request))
-
-
+	return redirect("/memeDetails/"+str(image_id))
