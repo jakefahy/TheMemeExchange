379fad4a9da1def67534f4180018891e5b11a583
Tracy Riordan <triordan112@gmail.com>
2020-03-24 21:36:22 -0700

    search functional

--- FrontPage/views.py
+++ FrontPage/views.py
@@ -2,9 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from dbFunctions import getLastTenImg
 from django.template import loader
+from dbFunctions import getImageByTag
 
 def index(request):
     context = {"images": getLastTenImg()}
     template = loader.get_template('front.html')
     return HttpResponse(template.render(context,request))
+    
+def search(request):
+    if request.method == 'GET':
+        term = request.GET['tag']
+        context = {"images": getImageByTag(term)}
+        template = loader.get_template('front.html')
+        return HttpResponse(template.render(context,request))
 # Create your views here.
