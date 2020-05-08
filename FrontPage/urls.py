5ea852a10983e38c0459f6f93b49592eba288461
ryan-beck <rbeck@csumb.edu>
2020-05-08 14:55:28 -0700

    unlocking images functional

--- FrontPage/urls.py
+++ FrontPage/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name = 'index'),
-    path('search/',views.searchByTag,name="searchByTag")
+    path('search/',views.searchByTag,name="searchByTag"),
+    path('unlock/', views.unlock, name='unlock')
     ]
--- FrontPage/views.py
+++ FrontPage/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render,redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from dbFunctions import getLastTenImg
 from django.template import loader
-from dbFunctions import getImageByTag, getImageByUser, getCreators, getViewedMemes, getImageByDescription
+from dbFunctions import getImageByTag, getImageByUser, getCreators, getViewedMemes, getImageByDescription, addToViewed
 
 def index(request):
     if request.user.is_anonymous:
@@ -28,7 +28,16 @@ def searchByTag(request):
                 context = {"images": getImageByTag(term)}
         except:
             context = {"images": getImageByDescription(request.GET['tag'])}
-            
-        
+
+
         template = loader.get_template('front.html')
         return HttpResponse(template.render(context,request))
+
+def unlock(request):
+    if request.is_ajax():
+        id = request.POST.get('memeid')
+        creator = request.POST.get('creator')
+        status = addToViewed(request.user, id, creator)
+        if status != -1:
+            request.session['coins'] = status
+        return JsonResponse({"status":status})
