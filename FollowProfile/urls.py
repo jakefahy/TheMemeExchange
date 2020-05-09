978915f6835798270444eb94cb95acdbfe0a2e9a
casscabrera <cass.cabrera99@gmail.com>
2020-05-08 19:11:05 -0700

    added unlocking image to profile page

--- FollowProfile/urls.py
+++ FollowProfile/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
 	path('<userID>/', views.index, name='index'),
 	path('userToBeFollowed/<userID>', views.userfollow, name='userfollow'),
 	path('userToBeUnfollowed/<userID>', views.userunfollow, name='userunfollow'),
+	path('<userID>/unlockM/', views.unlockM, name='unlockM')
 
 ]
